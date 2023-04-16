07e4a11c06b07d7293350110b95e36e875fb5bbc
richardcsuwandi <59959022+richardcsuwandi@users.noreply.github.com>
2023-04-16 21:08:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,18 +25,14 @@ def main():
         doc = nlp(text.lower())
         return " ".join([token.lemma_ for token in doc if not token.is_stop])
 
-    # Calculate the vector representation for each preprocessed title and description
-    data['vector_title'] = data['processed_title'].apply(lambda x: nlp(x).vector)
-    data['vector_description'] = data['Description'].apply(lambda x: nlp(x).vector)
+    def recommend_course(topic, num_to_rec=10):
+        # Calculate the vector representation for each preprocessed title and description
+        vector_matrix_title = pd.DataFrame(data['processed_title'].apply(lambda x: nlp(x).vector).tolist())
+        vector_matrix_description = pd.DataFrame(data['Description'].apply(lambda x: nlp(x).vector).tolist())
 
-       # Convert the vectors into matrices for cosine similarity calculation
-    vector_matrix_title = pd.DataFrame(data['vector_title'].tolist())
-    vector_matrix_description = pd.DataFrame(data['vector_description'].tolist())
-
-    def recommend_course(partial_title, num_of_rec=10):
-        # Preprocess and vectorize the input partial_title
-        preprocessed_partial_title = preprocess(partial_title)
-        input_vector = nlp(preprocessed_partial_title).vector
+        # Preprocess and vectorize the input topic
+        preprocessed_topic = preprocess(topic)
+        input_vector = nlp(preprocessed_topic).vector
 
         # Compute cosine similarity between the input_vector and course title vectors (Model 1)
         cosine_sim_titles = cosine_similarity([input_vector], vector_matrix_title)
@@ -45,29 +41,31 @@ def main():
         cosine_sim_descriptions = cosine_similarity([input_vector], vector_matrix_description)
 
         # Calculate the average cosine similarity
-        cosine_sim_avg = (cosine_sim_titles + cosine_sim_descriptions) / 2
+        cosine_sim_avg = 0.5*cosine_sim_titles + 0.5*cosine_sim_descriptions
 
         # Get the indices of recommended courses
         sorted_scores = sorted(enumerate(cosine_sim_avg[0]), key=lambda x: x[1], reverse=True)
-        selected_course_indices = [i[0] for i in sorted_scores[:num_of_rec]]
-        selected_course_scores = [i[1] for i in sorted_scores[:num_of_rec]]
+        selected_course_indices = [i[0] for i in sorted_scores[:num_to_rec]]
+        selected_course_scores = [i[1] for i in sorted_scores[:num_to_rec]]
 
         # Return the recommended courses along with their similarity scores
         result = raw_data['Code'].iloc[selected_course_indices]
         rec_df = pd.DataFrame(result)
         rec_df['Title'] = raw_data['Title'].iloc[selected_course_indices]
-        rec_df['similarity_scores'] = selected_course_scores
+        rec_df['Similarity Scores'] = selected_course_scores
         return rec_df
 
-    # Create a streamlit text input for the user to input a topic
+    st.markdown(" ### Recommended courses")
+
+    # Create a text input for the user to input a topic
     topic = st.sidebar.text_input("Enter a topic you are interested in")
 
     # Default number of recommended courses to 5
-    num_course = st.sidebar.slider("Number of courses to recommend", 1, 10, 5)
-    rec_df = recommend_course(topic, num_of_rec=num_course)
+    num_to_rec = st.sidebar.slider("Number of courses to recommend", 1, 10, 5)
 
     # Display the recommended courses
-    st.markdown(" ### Recommended courses")
+    rec_df = recommend_course(topic, num_to_rec)
+
     if topic:
         st.success("Here are some recommended courses for you:")
         st.write(rec_df)
@@ -75,7 +73,7 @@ def main():
         # Create a list from the recommended courses
         rec_list = rec_df['Code'].tolist()
 
-        # Create a streamlit select box for the user to select a course
+        # Create a select box for the user to select a course
         course = st.sidebar.selectbox("Select a course to see its description", rec_list)
 
         # Display the description of the selected course
